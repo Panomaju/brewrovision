29b0539e200805944532ef39fc1670238c7e165e
Matsuuu <huhta.matias@gmail.com>
2025-01-27 13:57:18 +0200

    Points change counts and counters

--- frontend/src/components/point-row.js
+++ frontend/src/components/point-row.js
@@ -1,10 +1,12 @@
 import { css, html, LitElement } from "lit";
+import { when } from "lit/directives/when.js";
 
 export class PointRow extends LitElement {
     static properties = {
         logo: { type: String },
         name: { type: String },
         points: { type: Number },
+        shownPoints: { type: Number },
         pointsChange: { type: Number },
     };
 
@@ -13,7 +15,35 @@ export class PointRow extends LitElement {
         this.logo = "";
         this.name = "";
         this.points = 0;
+        this.shownPoints = 0;
         this.pointsChange = undefined;
+    }
+
+    /**
+     * @param {import("lit").PropertyValues} _changedProperties
+     */
+    updated(_changedProperties) {
+        if (_changedProperties.has("points")) {
+            if (this.shownPoints < this.points) {
+                this.addShownPointWithDelay();
+            }
+            this.pointsChange = this.points - _changedProperties.get("points");
+        }
+        if (_changedProperties.has("shownPoints")) {
+            if (this.shownPoints < this.points) {
+                this.addShownPointWithDelay();
+            }
+        }
+    }
+
+    addShownPointWithDelay() {
+        const delay = 25;
+        setTimeout(
+            () => {
+                this.shownPoints += 1;
+            },
+            Math.floor(Math.random() * delay * 2) + delay,
+        );
     }
 
     render() {
@@ -26,7 +56,8 @@ export class PointRow extends LitElement {
             </div>
 
             <div class="right-side">
-                <p>${this.points}</p>
+                <p ?hidden=${!this.pointsChange} class="points-change">+ ${this.pointsChange}</p>
+                <p class="total-points">${this.shownPoints}</p>
             </div>
         `;
     }
@@ -47,6 +78,23 @@ export class PointRow extends LitElement {
             margin: 0;
             font-weight: bold;
             font-size: 1.6rem;
+        }
+
+        *[hidden] {
+            display: none;
+        }
+
+        .points-change {
+            padding: 0.5rem;
+            background: lightblue;
+            width: 4ch;
+            text-align: center;
+        }
+
+        .total-points {
+            padding: 0 0.5rem;
+            width: 3ch;
+            text-align: right;
         }
 
         .name-field {
